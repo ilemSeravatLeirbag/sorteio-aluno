54eeea90358ec5a1b8a1697ad39640d4ef58a24b
Gabriel Tavares <gabriel.tavares@mercadolivre.com>
2021-06-21 16:31:51 -0300

    first commit

---
+++ src/test/java/com/sorteiroalunos/SorteiroAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorteiroalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorteiroAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
